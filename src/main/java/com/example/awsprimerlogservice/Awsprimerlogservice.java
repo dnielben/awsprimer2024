b8083dcc5d9c36df4f8025eab8a494a1b4817576
Daniel Benavides <dnielben@gmail.com>
2024-03-05 15:57:29 -0500

    ¨

---
+++ src/main/java/com/example/awsprimerlogservice/Awsprimerlogservice.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.awsprimerlogservice;
+
+/**
+ *
+ * @author dnielben
+ */
+public class Awsprimerlogservice {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
